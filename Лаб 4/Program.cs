82f45710f88cee20e6eadf0312142a0eb5e93bfe
Garten53 <92727647+Garten53@users.noreply.github.com>
2021-12-23 16:52:12 +0200

    Add files via upload

---
+++ Лаб 4/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp134
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            LinkedList MyList = new LinkedList();
+            MyList.AddLast(20);
+            MyList.AddLast(13);
+            MyList.AddLast(88);
+            MyList.AddAfterMax(99);
+            MyList.AddAfterMax(95);
+            MyList.AddAfterMax(100);
+            MyList.PrintList();
+            Console.ReadKey();
+        }
+    }
+}
