ad26b1af8fa579e4fce87806e031f7f480edf336
Garten53 <92727647+Garten53@users.noreply.github.com>
2022-05-29 23:44:55 +0300

    Add files via upload

---
+++ Лаб5/Program143.cs
@@ -0,0 +1,54 @@
+﻿using System;
+
+namespace TestSort
+{
+    class Program
+    {
+        static int[] Array;
+        static int N;
+        static void Main(string[] args)
+        {
+            N = 10;
+            Console.WriteLine("Input N: " + N.ToString());
+
+            InitArray();
+            PrintArray();
+            Sorter(0, N / 2, 1);
+            Sorter(N / 2, N, -1);
+            PrintArray();
+
+
+           
+
+        }
+
+        static private void InitArray()
+        {
+            Array = new int[N];
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+                Array[i] = rnd.Next(1, 100);
+        }
+
+        static private void PrintArray()
+        {
+            Random rnd = new Random();
+            for (int i = 0; i < N; i++)
+                Console.Write(Array[i].ToString() + " ");
+            Console.WriteLine();
+        }
+
+        static private void Sorter(int iLeft, int iRight, int flag)
+        {
+            for (int i = 0; i < iRight - iLeft; i++)
+                for (int j = iLeft; j < iRight - 1; j++)
+                    if (flag * (Array[j] - Array[j + 1]) > 0)
+                    {
+                        int tmp = Array[j];
+                        Array[j] = Array[j + 1];
+                        Array[j + 1] = tmp;
+                    }
+        }
+
+    }
+}
---
+++ Лаб5/Program2.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            var arr = GetRandomArray(10, 1, 50);
+            Console.WriteLine("Вхідні дані: {0}", string.Join(", ", arr));
+            Console.WriteLine("Впорядкований масив: {0}", string.Join(", ", BasicCountingSort(arr, 50)));
+            Console.ReadLine();
+        }
+        
+        static int[] BasicCountingSort(int[] array, int k)
+        {
+            var count = new int[k + 1];
+            for (var i = 0; i < array.Length; i++)
+            {
+                count[array[i]]++;
+            } 
+
+            var index = 0;
+            for (var i = 0; i < count.Length; i++)
+            {
+                for (var j = 0; j < count[i]; j++)
+                {
+                    array[index] = i;
+                    index++;
+                }
+            }
+
+            return array;
+           
+        }
+        
+        static int[] GetRandomArray(int arraySize, int minValue, int maxValue)
+        {
+            var random = new Random();
+            var randomArray = new int[arraySize];
+            for (var i = 0; i < randomArray.Length; i++)
+            {
+                randomArray[i] = random.Next(minValue, maxValue);
+            }
+
+            return randomArray;
+        }
+    }
+}
